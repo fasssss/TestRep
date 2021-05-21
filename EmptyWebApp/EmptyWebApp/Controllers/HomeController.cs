0ad9ac16ac269e91977be755ea02d00729f58051
fasssss <buinsky.andrei@gmail.com>
2021-05-22 01:34:10 +0300

    Login and Register was added

---
+++ EmptyWebApp/EmptyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EmptyWebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly SignInManager<IdentityUser> _signInManager;
+
+		public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Authorize]
+		public IActionResult Secret()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(string username, string password)
+		{
+			var user = await _userManager.FindByNameAsync(username);
+			if (user != null)
+			{
+				var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+				if (signInResult.Succeeded)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(string username, string password)
+		{
+			var user = new IdentityUser
+			{
+				UserName = username
+			};
+
+			var result = await _userManager.CreateAsync(user, password);
+
+			if (result.Succeeded)
+			{
+				var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+				if (signInResult.Succeeded)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Login()
+		{
+
+			return View();
+		}
+
+		public IActionResult Register()
+		{
+
+			return View();
+		}
+
+		public async Task<IActionResult> LogOut()
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Index");
+		}
+	}
+}
