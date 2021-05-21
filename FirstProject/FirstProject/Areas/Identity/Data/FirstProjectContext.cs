389354df07f1a23ab9a1227add7cd789fc3e2b0d
fasssss <buinsky.andrei@gmail.com>
2021-05-21 11:36:32 +0300

    Another way to login also desn't work

---
+++ FirstProject/FirstProject/Areas/Identity/Data/FirstProjectContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstProject.Data
+{
+    public class FirstProjectContext : IdentityDbContext<IdentityUser>
+    {
+        public FirstProjectContext(DbContextOptions<FirstProjectContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
