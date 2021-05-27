b5b3873a82c45aa8da8a07bb9e992caa5b8ee990
JustRetard <buinsky.andrei@gmail.com>
2021-05-27 16:22:29 +0300

    Private Info was refactored to MVC

---
+++ FirstProject/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstProject.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly ILogger<HomeController> _logger;
+		private readonly SignInManager<IdentityUser> _signInManager;
+		public HomeController(SignInManager<IdentityUser> signInManager,
+			ILogger<HomeController> logger,
+			UserManager<IdentityUser> userManager)
+		{
+			_logger = logger;
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> PrivateInfo()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			var claims = User.Claims;
+			PrivateInfoModel model = new PrivateInfoModel(user);
+			if (user != null)
+			{
+
+				model.currentLogins = await _userManager.GetLoginsAsync(user);
+				foreach (var claim in claims)
+				{
+					model.claims.Add(new PrivateInfoModel.OutputFormatClaims
+					{
+						Name = claim.Subject.Name,
+						Issuer = claim.Issuer,
+						Type = claim.Type.ToString().Substring(claim.Type.ToString().LastIndexOfAny(new char[] { '/', '.' }) + 1),
+						Value = claim.Value
+					});
+				}
+				model.IsAuthenticated = true;
+			}
+			else
+			{
+				model.IsAuthenticated = false;
+			}
+			return View(model);
+		}
+
+		[AllowAnonymous]
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ FirstProject/FirstProject/Models/PrivateInfoModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProject.Models
+{
+	public class PrivateInfoModel
+	{
+		public IList<UserLoginInfo> currentLogins { get; set; }
+		public IdentityUser user { get; }
+		public List<OutputFormatClaims> claims { get; set; }
+		public bool IsAuthenticated { get; set; }
+		public PrivateInfoModel(IdentityUser user = null) : this()
+		{
+			this.user = user;
+		}
+		public PrivateInfoModel()
+		{
+			currentLogins = new List<UserLoginInfo>();
+			claims = new List<OutputFormatClaims>();
+		}
+		public class OutputFormatClaims
+		{
+			public string Name { get; set; }
+			public string Issuer { get; set; }
+			public string Type { get; set; }
+			public string Value { get; set; }
+		}
+	}
+}
